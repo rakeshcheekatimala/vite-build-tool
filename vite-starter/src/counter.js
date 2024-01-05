055a842a56bd0f4957c2a2623ad4187b9b9e27d4
rakeshcheekatimala <rakeshcheekatimala@gmail.com>
2024-01-05 11:32:41 +0530

    adding postcss prefixer

--- vite-starter/src/counter.js
+++ vite-starter/src/counter.js
@@ -1,3 +1,5 @@
+import './postcss.css';
+
 export const initializeCounter = (doc = globalThis.document) => {
   const countElement = doc.getElementById('count');
   const incrementButton = doc.getElementById('increment');
@@ -7,6 +9,19 @@ export const initializeCounter = (doc = globalThis.document) => {
 
   const render = () => {
     countElement.textContent = count;
+    if (count < 0) {
+      import('./banner').then(({ addBanner }) => {
+        addBanner('Counter value is less than zero');
+      });
+    } else if (count > 10) {
+      import('./banner').then(({ addBanner }) => {
+        addBanner('Counter value is greater than 10');
+      });
+    } else {
+      import('./banner').then(({ addBanner }) => {
+        addBanner('');
+      });
+    }
   };
 
   const increment = () => {
